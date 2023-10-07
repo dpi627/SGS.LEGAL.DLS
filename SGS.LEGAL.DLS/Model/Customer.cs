18af269248b25bad2e6fb2ebc03fd6a43e1a41b3
Brian <brian.li@sgs.com>
2023-10-07 16:12:00 +0800

    fet: 完成存證信函郵遞區號拆分

--- SGS.LEGAL.DLS/Model/Customer.cs
+++ SGS.LEGAL.DLS/Model/Customer.cs
@@ -26,10 +26,10 @@ namespace SGS.LEGAL.DLS.Model
         /// <summary>
         /// 郵遞區號前三碼
         /// </summary>
-        public string? CST_POST_CODE_1 => CST_POST_CODE?.Substring(0,3);
+        public string? CST_POST_CODE_1 => CST_POST_CODE?[..3];
         /// <summary>
         /// 郵遞區號第四碼到最後
         /// </summary>
-        public string? CST_POST_CODE_2 => CST_POST_CODE?.Substring(3);
+        public string? CST_POST_CODE_2 => CST_POST_CODE?[3..];
     }
 }
